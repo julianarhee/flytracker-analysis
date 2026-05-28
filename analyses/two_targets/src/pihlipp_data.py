f3d45fafe8ca33431abb017c6b3437ec90248548
Juliana Rhee <juliana.rhee@gmail.com>
2026-05-28 14:45:09 -0400

    two_targets: disable unused add_pairwise_metrics helper in pihlipp_data

--- analyses/two_targets/src/pihlipp_data.py
+++ analyses/two_targets/src/pihlipp_data.py
@@ -145,22 +145,22 @@ yak_acqs = [acq for acq in acqs if 'yak' in acq]
 # ax.set_title(acq)
 # %%
 # Transform data
-def add_pairwise_metrics(trk_, feat_, calib):
-    ppm = calib.get('PPM', 1)
-    for fid, oid in [(flyid1, flyid2), (flyid2, flyid1)]:
-        f_trk = trk_[trk_['id']==fid]
-        o_trk = trk_[trk_['id']==oid]
-        dist = util.compute_dist_to_other(
-            f_trk['pos_x'].values, f_trk['pos_y'].values,
-            o_trk['pos_x'].values, o_trk['pos_y'].values,
-            pix_per_mm=ppm)
-        feat_.loc[feat_['id']==fid, 'dist_to_other'] = dist
-        facing_angle = util.compute_facing_angle(
-            f_trk['ori'].values, f_trk['pos_x'].values, f_trk['pos_y'].values,
-            o_trk['pos_x'].values, o_trk['pos_y'].values)
-        feat_.loc[feat_['id']==fid, 'facing_angle'] = facing_angle
-
-    return trk_, feat_
+#def add_pairwise_metrics(trk_, feat_, calib):
+#    ppm = calib.get('PPM', 1)
+#    for fid, oid in [(flyid1, flyid2), (flyid2, flyid1)]:
+#        f_trk = trk_[trk_['id']==fid]
+#        o_trk = trk_[trk_['id']==oid]
+#        dist = util.compute_dist_to_other(
+#            f_trk['pos_x'].values, f_trk['pos_y'].values,
+#            o_trk['pos_x'].values, o_trk['pos_y'].values,
+#            pix_per_mm=ppm)
+#        feat_.loc[feat_['id']==fid, 'dist_to_other'] = dist
+#        facing_angle = util.compute_facing_angle(
+#            f_trk['ori'].values, f_trk['pos_x'].values, f_trk['pos_y'].values,
+#            o_trk['pos_x'].values, o_trk['pos_y'].values)
+#        feat_.loc[feat_['id']==fid, 'facing_angle'] = facing_angle
+#
+#    return trk_, feat_
 
 def plot_switch_bouts(f1, frames, switch_positions, fps, species='', acq='',
                       n_sec_pre=1.0, n_sec_post=1.0, n_plots=None, 
